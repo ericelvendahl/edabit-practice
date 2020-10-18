4151b6b544e90b60b4f07779d6b109fa1ab786a9
“ericelvendahl” <ericelvendahl@gmail.com>
2020-10-18 09:46:58 -0500

    hasKey working

--- edabit.js
+++ edabit.js
@@ -1,2 +1,8 @@
 // Edabit
-console.log("Hello, World!!");
+const myObject = { one: 1, two: 2, three: 3 };
+
+function hasKey(obj, key) {
+  return key in obj;
+}
+
+console.log(hasKey(myObject, "three"));
