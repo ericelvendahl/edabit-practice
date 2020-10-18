1c19732f21d280a6dd00a768fce7a1174b034cdb
“ericelvendahl” <ericelvendahl@gmail.com>
2020-10-18 12:40:06 -0500

    checkIfSameCase working

---
+++ checkIfTheSameCase.js
@@ -0,0 +1,20 @@
+// Check if the Same Case
+// Create a function that returns true if an input string contains only uppercase or only lowercase letters.
+
+// Examples
+// sameCase("hello") ➞ true
+
+// sameCase("HELLO") ➞ true
+
+// sameCase("Hello") ➞ false
+
+// sameCase("ketcHUp") ➞ false
+
+function sameCase(str) {
+  return str === str.toUpperCase() || str === str.toLowerCase();
+}
+
+console.log(sameCase("hello"));
+console.log(sameCase("HELLO"));
+console.log(sameCase("Hello"));
+console.log(sameCase("ketcHUp"));
